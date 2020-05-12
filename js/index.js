65ae221a271e92af5f70b7379635abfb3cf28eac
lukeouyang <luke_ou@hotmail.com>
2020-05-12 03:43:14 -0700

    fix the sliding navbar-tip position matching issue, other minor issues too.

--- js/index.js
+++ js/index.js
@@ -1,44 +1,44 @@
-// 获取元素
+// set up variables for elements
 
-var getElem = function( selector ){
+const getElem = function (selector) {
   return document.querySelector(selector);
-}
-var getAllElem = function( selector ){
+};
+const getAllElem = function (selector) {
   return document.querySelectorAll(selector);
-}
-// 获取元素的样式
-var getCls = function ( element ) {
+};
+// get class for elements
+const getCls = function (element) {
   return element.getAttribute('class');
-}
-// 设置元素的样式
-var setCls = function( element ,cls){
-  return element.setAttribute('class',cls);
-}
+};
+// set class for elements
+const setCls = function (element, cls) {
+  return element.setAttribute('class', cls);
+};
 
-// 为元素添加样式
-var addCls = function( element , cls ){
-  var baseCls  = getCls(element);
-  if( baseCls.indexOf(cls) === -1){
-      setCls(element,baseCls+' '+cls); // 注意空格
+// add class to elements
+const addCls = function (element, cls) {
+  const baseCls = getCls(element);
+  if (baseCls.indexOf(cls) === -1) {
+    setCls(element, baseCls + ' ' + cls); // add space in between classes
   }
-  return ;
-}
-// 为元素删减样式
-var delCls = function( element , cls){
-  var baseCls  = getCls(element);
-  if( baseCls.indexOf(cls) > -1){ // 更精确的需要用正则表达式 ,因为这里只用于切换 _animate_in 所以没事
-      setCls( element,baseCls.split(cls).join(' ').replace(/\s+/g,' ') );
+  return;
+};
+// remove class for elements
+const delCls = function (element, cls) {
+  const baseCls = getCls(element);
+  if (baseCls.indexOf(cls) > -1) {
+    setCls(element, baseCls.split(cls).join(' ').replace(/\s+/g, ' '));
   }
-  return ;
-}
+  return;
+};
 
-var screenAnimateElements = {
-  '.screen-1' : [
+const screenAnimateElements = {
+  '.screen-1': [
     '.screen-1__heading',
     '.screen-1__phone',
     '.screen-1__shadow',
   ],
-  '.screen-2' : [
+  '.screen-2': [
     '.screen-2__heading',
     '.screen-2__subheading',
     '.screen-2__phone',
@@ -46,13 +46,13 @@ var screenAnimateElements = {
     '.screen-2__point_i_2',
     '.screen-2__point_i_3',
   ],
-  '.screen-3' : [
+  '.screen-3': [
     '.screen-3__heading',
     '.screen-3__phone',
     '.screen-3__subheading',
     '.screen-3__features',
   ],
-  '.screen-4' : [
+  '.screen-4': [
     '.screen-4__heading',
     '.screen-4__subheading',
     '.screen-4__type__item_i_1',
@@ -60,94 +60,99 @@ var screenAnimateElements = {
     '.screen-4__type__item_i_3',
     '.screen-4__type__item_i_4',
   ],
-  '.screen-5' : [
-     '.screen-5__heading',
+  '.screen-5': [
+    '.screen-5__heading',
     '.screen-5__subheading',
     '.screen-5__bg',
   ]
 
 };
+
 function setScreenAnimateInit(screenCls) {
-    var screen = document.querySelector(screenCls); // 获取当前屏的元素
-    var animateElements =  screenAnimateElements[screenCls]; // 需要设置动画的元素
-    for(var i=0;i<animateElements.length;i++){
-        var element = document.querySelector(animateElements[i]);
-        var baseCls = element.getAttribute('class');
-        element.setAttribute('class',baseCls +' '+animateElements[i].substr(1)+'_animate_init');
+  const screen = document.querySelector(screenCls); // get the screen element
+  const animateElements = screenAnimateElements[screenCls]; // get the elements for animation
+    for(let i=0; i<animateElements.length; i++){
+      const element = document.querySelector(animateElements[i]);
+      const baseCls = element.getAttribute('class');
+      element.setAttribute('class',baseCls +' '+animateElements[i].substr(1)+'_animate_init');
     }
 }
 
-// 第一步：初始化设置
+// Step1: initialization
 window.onload = function () {
 
-  //  为所有元素设置 init
-  for(k in screenAnimateElements){
-    if(k == '.screen-1'){
+  //  set init for all elements
+  for(let k in screenAnimateElements){
+    if(k === '.screen-1'){
       continue;
     }
     setScreenAnimateInit(k);
   }
-  console.log('onload')
+  // console.log('onload')
 
-}
-// 第二步：滚动条设置
+};
+// step2：set the scroll bar
 function playScreenAnimateDone(screenCls){
-    var screen = document.querySelector(screenCls); // 获取当前屏的元素
-    var animateElements =  screenAnimateElements[screenCls]; // 需要设置动画的元素
-    for(var i=0;i<animateElements.length;i++){
-        var element = document.querySelector(animateElements[i]);
-        var baseCls = element.getAttribute('class');
-        element.setAttribute('class',baseCls.replace('_animate_init','_animate_done'));    
+  const screen = document.querySelector(screenCls); // get the screen element
+  const animateElements = screenAnimateElements[screenCls]; // get all the elements need to animate
+    for(let i=0; i<animateElements.length; i++){
+      const element = document.querySelector(animateElements[i]);
+      const baseCls = element.getAttribute('class');
+      element.setAttribute('class',baseCls.replace('_animate_init','_animate_done'));
     }
 }
-//  第二步附加：初始化第一屏的动画（1. skipScreenAnimateInit 2.跳过 init ）
+//  Step2-sub：initialize section 1 animation（1. skipScreenAnimateInit 2.skip init ）
 
-setTimeout(function(){playScreenAnimateDone('.screen-1');},100)
+setTimeout(function(){playScreenAnimateDone('.screen-1');},100);
 
 
-var navItems = getAllElem('.header__nav-item');
-var outLineItems = getAllElem('.outline__item');
+const navItems = getAllElem('.header__nav-item');
+const outLineItems = getAllElem('.outline__item');
 
-var switchNavItemsActive = function( idx){
-  for(var i=0;i<navItems.length;i++){
-    console.log(navItems[i]);
-    delCls(navItems[i],'header__nav-item_status_active');
-     navTip.style.left = 0+'px';
-    
+
+// 滑动门
+const navTip = getElem('.header__nav-tip');
+const switchNavItemsActive = function (idx) {
+  let i;
+  for (i = 0; i < navItems.length; i++) {
+    // console.log(navItems[i]);
+    delCls(navItems[i], 'header__nav-item_status_active');
+    navTip.style.left = 0 + 'px';
+
   }
-  addCls(navItems[idx],'header__nav-item_status_active');
-  navTip.style.left = ( idx * 70 )+'px';
-  
+  addCls(navItems[idx], 'header__nav-item_status_active');
+  navTip.style.left = (idx * 70) + 'px';
 
-  for(var i=0;i<outLineItems.length;i++){
-    delCls(outLineItems[i],'outline__item_status_active');
+
+  for (i = 0; i < outLineItems.length; i++) {
+    delCls(outLineItems[i], 'outline__item_status_active');
   }
-  addCls(outLineItems[idx],'outline__item_status_active');
-}
+  addCls(outLineItems[idx], 'outline__item_status_active');
+};
 
 window.onscroll = function () {
 
   var top  = document.body.scrollTop;
 
-  //   2.1 导航条样式变动
+  //   2.1 nav bar appearance change
   if( top > 100 ){
       addCls( getElem('.header'),'header_status_black' );
   }else{
       delCls( getElem('.header'),'header_status_black' );
 
-      switchNavItemsActive(0); // 后面添加的，不需要立刻
+      switchNavItemsActive(0);
   }
 
-  if(top > 800*1 ){
+  if(top > 800 ){
       addCls( getElem('.outline'),'outline_status_in' );}
   // }else{
   //     delCls( getElem('.outline'),'outline_status_in' );
   // }
 
-  if( top > ( 800*1 - 100) ){
+  if( top > ( 800 - 100) ){
     playScreenAnimateDone('.screen-2');
 
-    switchNavItemsActive(1); // 后面添加的，不需要立刻
+    switchNavItemsActive(1);
   }
   if( top > ( 800*2 - 100) ){
     playScreenAnimateDone('.screen-3');
@@ -161,54 +166,50 @@ window.onscroll = function () {
     playScreenAnimateDone('.screen-5');
     switchNavItemsActive(4); 
   }
-}
+};
 
-//  第三步 导航条双向定位
+//  Step 3 Binding of the main navigation bar and the floating shortcut navigation
 
-// 3.1 导航条 - 点击页面跳转
+// 3.1 main navigation bar - jump to sections
 
-var setNavJump = function(i,lib){
-  var elem = lib[i];
-  elem.onclick = function(){
-    document.body.scrollTop = i*800 + 1;
+const setNavJump = function (i, lib) {
+  const elem = lib[i];
+  elem.onclick = function () {
+    document.body.scrollTop = i * 800 + 1;
   }
-}
+};
 
-for(var i=0;i<navItems.length;i++){
+let i;
+for(i = 0; i<navItems.length; i++){
   setNavJump(i,navItems);
 }
-// 3.2  大纲-点击跳转
+// 3.2  floating nav - jump to sections
 
-for(var i=0;i<outLineItems.length;i++){
+for(i = 0; i<outLineItems.length; i++){
   setNavJump(i,outLineItems);
 }
-// 3.3 双向绑定，回到 onscrollTop（移动 navIntes、outLineItems到顶固）、增加 clear 样式 函数
+// 3.3 two-way binding，back to onscrollTop（move navIntes、outLineItems to top）、add clear css function
+const setTip = function (idx, lib) {
 
 
-// 滑动门
-var navTip = getElem('.header__nav-tip');
-var setTip = function(idx,lib){
-
-  
-
-  lib[idx].onmouseover =function(){
-    console.log(this,idx);
-    navTip.style.left = ( idx * 70 )+'px';
-  }
-  var currentIdx = 0;
-  lib[idx].onmouseout = function(){
-    console.log(currentIdx);
-    for(var i=0;i<lib.length;i++){
-        if( getCls( lib[i] ).indexOf('header__nav-item_status_active') > -1  ){
-          currentIdx = i;
-          break;
-        }
+  lib[idx].onmouseover = function () {
+    // console.log(this, idx);
+    navTip.style.left = (idx * 76) + 'px';
+  };
+  let currentIdx = 0;
+  lib[idx].onmouseout = function () {
+    // console.log(currentIdx);
+    for (let i = 0; i < lib.length; i++) {
+      if (getCls(lib[i]).indexOf('header__nav-item_status_active') > -1) {
+        currentIdx = i;
+        break;
+      }
     }
-    navTip.style.left = ( currentIdx * 70 )+'px';
+    navTip.style.left = (currentIdx * 76) + 'px';
   }
 
-}
+};
 
-for(var i=0;i<navItems.length;i++){
+for(i = 0; i<navItems.length; i++){
   setTip(i,navItems);
 }
